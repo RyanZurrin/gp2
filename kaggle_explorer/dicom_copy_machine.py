e2e5bd7c3f2904a3119dfb4b0c1bea914ed5a53b
RyanZurrin <ryanzurrin@ryanzurrin.com>
2023-02-24 16:15:24 -0500

    got the kaggle images viewable after switching header info

--- kaggle_explorer/dicom_copy_machine.py
+++ kaggle_explorer/dicom_copy_machine.py
@@ -44,17 +44,65 @@ class DicomCopyMachine:
             The new dicom
         """
         new_dicom = self.dicom_A
-        new_dicom.PixelData = self.dicom_B.PixelData
+
 
         # TODO: crashes kernal, figure out if there is other way to interpret
         #  pixel data properly
-        # new_dicom.file_meta.TransferSyntaxUID = \
-        #     self.dicom_B.file_meta.TransferSyntaxUID
+        new_dicom.file_meta.TransferSyntaxUID = \
+            self.dicom_B.file_meta.TransferSyntaxUID
+        # set all the (0028, XXXX) tags to the same as dicom B
+        # samples per pixel
+        new_dicom.SamplesPerPixel = self.dicom_B.SamplesPerPixel
+        # photometric interpretation
+        new_dicom.PhotometricInterpretation = \
+            self.dicom_B.PhotometricInterpretation
+        # rows
+        new_dicom.Rows = self.dicom_B.Rows
+        # columns
+        new_dicom.Columns = self.dicom_B.Columns
+        # bits allocated
+        new_dicom.BitsAllocated = self.dicom_B.BitsAllocated
+        # bits stored
+        new_dicom.BitsStored = self.dicom_B.BitsStored
+        # high bit
+        new_dicom.HighBit = self.dicom_B.HighBit
+        # pixel representation
+        new_dicom.PixelRepresentation = self.dicom_B.PixelRepresentation
+        # pixel padding value
+        new_dicom.PixelPaddingValue = self.dicom_B.PixelPaddingValue
+        # pixel intensity relationship
+        new_dicom.PixelIntensityRelationship = \
+            self.dicom_B.PixelIntensityRelationship
+        # pixel intensity relationship sign
+        new_dicom.PixelIntensityRelationshipSign = \
+            self.dicom_B.PixelIntensityRelationshipSign
+        # window center
+        new_dicom.WindowCenter = self.dicom_B.WindowCenter
+        # window width
+        new_dicom.WindowWidth = self.dicom_B.WindowWidth
+        # rescale intercept
+        new_dicom.RescaleIntercept = self.dicom_B.RescaleIntercept
+        # rescale slope
+        new_dicom.RescaleSlope = self.dicom_B.RescaleSlope
+        # rescale type
+        new_dicom.RescaleType = self.dicom_B.RescaleType
+        # VOI LUT Function
+        new_dicom.VOILUTFunction = self.dicom_B.VOILUTFunction
+        # partial view
+        new_dicom.PartialView = self.dicom_B.PartialView
+        # Lossy Image Compression
+        new_dicom.LossyImageCompression = self.dicom_B.LossyImageCompression
+
+        # remove pixel padding range limit
+        if hasattr(new_dicom, 'PixelPaddingRangeLimit'):
+            del new_dicom.PixelPaddingRangeLimit
+
+        # remove VOI LUT Sequence
+        if hasattr(new_dicom, 'VOILUTSequence'):
+            del new_dicom.VOILUTSequence
 
 
-        # add the window center and width from B to C
-        # new_dicom.WindowCenter = self.dicom_B.WindowCenter
-        # new_dicom.WindowWidth = self.dicom_B.WindowWidth
+        new_dicom.PixelData = self.dicom_B.PixelData
 
         if self.save_path is not None:
             new_dicom.save_as(self.save_path)
--- omama/__init__.py
+++ omama/__init__.py
@@ -1,5 +1,2 @@
-from .outlier_detector import *
-from .feature_extractor import *
 from .deepsight import *
 from .data import *
-from .hyperparameter_tuner import *
