3c330873e772c9c7f56d7e5fe4d00b32c77d7a7e
RyanZurrin <ryanzurrin@ryanzurrin.com>
2023-05-03 02:44:42 -0400

    added proper docstrings to the gp2.data module files and updated the API documentation to include updates.

--- gp2/data/collection.py
+++ gp2/data/collection.py
@@ -4,28 +4,61 @@ from .point import Point
 
 
 class Collection:
+    """ A collection of DataPoints
+    """
 
     def __init__(self):
         self.id = str(uuid4())
         self.data = {}
 
     def add(self, datapoint):
+        """ Add a DataPoint to the collection
+
+        Parameters
+        ----------
+        datapoint
+            The DataPoint to add
+
+        """
         self.data[datapoint.id] = datapoint.value
 
     def remove(self, datapoint):
         self.data.pop(datapoint.id)
-        
+
     def get(self, index):
-        '''
-        '''
+        """ Get a DataPoint from the specified index in the collection
+
+        Parameters
+        ----------
+        index : int
+            The index of the DataPoint to get
+
+        Returns
+        -------
+        DataPoint
+            The DataPoint at the specified index
+        """
         return self.data[list(self.data.keys())[index]]
 
-    def get_point(self, id):
+    def get_point(self, _id):
+        """ Get a DataPoint from the specified id in the collection
 
-        return self.data[id]
+        Parameters
+        ----------
+        _id : str
+            The id of the DataPoint to get
+
+        Returns
+        -------
+        DataPoint
+            The DataPoint with the specified id
+        """
+
+        return self.data[_id]
 
     def shuffle(self):
-
+        """ Shuffle the collection
+        """
         all_ids = list(self.data.keys())
         np.random.shuffle(all_ids)
 
@@ -34,17 +67,16 @@ class Collection:
             shuffled_data[k] = self.data[k]
 
         self.data = shuffled_data
-        
 
     def to_array(self):
-        '''
-        '''
+        """ Convert the collection to a numpy array
+        """
 
         datasize = len(self.data.keys())
         datashape = self.get(0).shape
         datatype = self.get(0).dtype
 
-        a = np.zeros(((datasize,)+datashape), dtype=datatype)
+        a = np.zeros(((datasize,) + datashape), dtype=datatype)
         ids = []
 
         id_list = list(self.data.keys())
@@ -58,25 +90,34 @@ class Collection:
 
     @staticmethod
     def from_point_list(datalist):
-        '''
-        dataList needs to be a list of DataPoints
-        '''
+        """ dataList needs to be a list of DataPoints
+
+        Parameters
+        ----------
+        datalist : list
+            A list of DataPoints
+        """
         c = Collection()
 
         for i in datalist:
-
             c.add(i)
 
         return c
 
     @staticmethod
     def from_list(datalist, keys=None):
-        '''
-        datalist can be anything
-        '''
+        """ datalist can be anything
+
+        Parameters
+        ----------
+        datalist : list
+            A list of DataPoints
+        keys : list
+            A list of keys to use for the DataPoints
+        """
         c = Collection()
 
-        for i,v in enumerate(datalist):
+        for i, v in enumerate(datalist):
 
             p = Point(v)
 
@@ -89,4 +130,3 @@ class Collection:
 
     def __str__(self):
         return str(self.data)
-
--- gp2/data/manager.py
+++ gp2/data/manager.py
@@ -3,14 +3,24 @@ from .point import Point
 
 import pickle
 
+
 class Manager:
+    """ A class to manage collections of data."""
 
     def __init__(self):
+        """ Initialize the manager."""
         self.collections = {}
         self.names = {}
 
     def register(self, collection, name=None):
-
+        """ Register a collection with the manager.
+        Parameters
+        ----------
+        collection : Collection
+            The collection to register.
+        name : str
+            The name to register the collection with.
+        """
         # here we need to remove the old collection
         if name:
             for c in self.names:
@@ -23,12 +33,21 @@ class Manager:
 
         # register this collection + name if given
         self.collections[collection.id] = collection
-        
+
         if name:
             self.names[collection.id] = name
 
     def get(self, name):
-
+        """ Get a collection by name.
+        Parameters
+        ----------
+        name : str
+            The name of the collection to get.
+        Returns
+        -------
+        Collection
+            The collection with the given name.
+        """
         # get name
         for c in self.names:
             if self.names[c] == name:
@@ -36,34 +55,82 @@ class Manager:
 
     @staticmethod
     def add(collection, datapoint):
-
+        """ Add a datapoint to a collection.
+        Parameters
+        ----------
+        collection : Collection
+            The collection to add the datapoint to.
+        datapoint : Point
+            The datapoint to add.
+        Returns
+        -------
+        Collection
+            The collection with the added datapoint.
+        """
         collection.add(datapoint)
         return collection
 
     @staticmethod
     def remove(collection, datapoint):
-
+        """ Remove a datapoint from a collection.
+        Parameters
+        ----------
+        collection : Collection
+            The collection to remove the datapoint from.
+        datapoint : Point
+            The datapoint to remove.
+        Returns
+        -------
+        Collection
+            The collection with the removed datapoint.
+        """
         collection.remove(datapoint)
         return collection
 
     @staticmethod
     def remove_and_add(old_collection, new_collection, datapoint):
-        
+        """ Remove a datapoint from one collection and add it to another.
+        Parameters
+        ----------
+        old_collection : Collection
+            The collection to remove the datapoint from.
+        new_collection : Collection
+            The collection to add the datapoint to.
+        datapoint : Point
+            The datapoint to remove and add.
+        Returns
+        -------
+        Collection
+            The old collection with the removed datapoint.
+        Collection
+            The new collection with the added datapoint.
+        """
         old_collection.remove(datapoint)
         new_collection.add(datapoint)
 
         return old_collection, new_collection
 
     def find(self, datapoint_id, show_names=False):
-
+        """ Find a collection by datapoint id.
+        Parameters
+        ----------
+        datapoint_id : int
+            The id of the datapoint to find.
+        show_names : bool
+            Whether to return the collection names instead of the collections.
+        Returns
+        -------
+        list
+            The collections containing the datapoint.
+        """
         collections = []
 
         # also support point objects
-        if type(datapoint_id)==Point:
+        if type(datapoint_id) == Point:
             datapoint_id = datapoint_id.id
 
         for c in self.collections:
-            
+
             if datapoint_id in self.collections[c].data:
                 collections.append(self.collections[c])
 
@@ -71,27 +138,39 @@ class Manager:
         collections_with_names = {}
 
         if show_names:
-            for i,c in enumerate(collections):
+            for i, c in enumerate(collections):
 
                 if collections[i].id in self.names:
                     name = self.names[collections[i].id]
                 else:
-                    name = 'unknown'+str(i)
+                    name = 'unknown' + str(i)
                 collections_with_names[name] = collections[i]
             collections = collections_with_names
 
-
         return collections
 
-
     def save(self, filepath):
-
+        """ Save the manager to a file.
+        Parameters
+        ----------
+        filepath : str
+            The filepath to save the manager to.
+        """
         with open(filepath, 'wb') as f:
             pickle.dump(self, f)
 
     @staticmethod
     def load(filepath):
-
+        """ Load a manager from a file.
+        Parameters
+        ----------
+        filepath : str
+            The filepath to load the manager from.
+        Returns
+        -------
+        Manager
+            The loaded manager.
+        """
         with open(filepath, 'rb') as f:
             m = pickle.load(f)
 
